f6444b00ad872268a3f3e485b7b8e6d560f53a34
Faiyaz Haider <faiyaz7283@gmail.com>
2020-10-29 01:34:56 -0400

    simple change

--- sys_status.py
+++ sys_status.py
@@ -20,7 +20,7 @@ i2c = busio.I2C(SCL, SDA)
 # to the right size for your display!
 disp = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
 
-# Clear display.
+# Clear the display.
 disp.fill(0)
 disp.show()
 
